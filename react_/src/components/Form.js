7b79c1265152b0d5105dc4b629fb1ab082ff0748
Aproximo <Romanyk94@gmail.com>
2018-02-09 13:15:28 +0200

    post axios

--- react_/src/components/Form.js
+++ react_/src/components/Form.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import '../style/Form.css';
+import axios from 'axios'
 
 class Form extends Component {
     constructor(props) {
@@ -18,6 +19,7 @@ class Form extends Component {
         this.handlePassportPassChange = this.handlePassportPassChange.bind(this);
         this.handleIdCodeChange = this.handleIdCodeChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
     handlePassIdChange(e) {
@@ -53,18 +55,49 @@ class Form extends Component {
     handleSubmit(e) {
        e.preventDefault();
        console.log(this.state)
+
+        axios.post('127.0.0.1:8000/api/form', {
+            firstName: 'Fred',
+            lastName: 'Flintstone'
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log("error", error);
+            });
     }
+
+
 
     render() {
         return (
             <form onSubmit={this.handleSubmit} className="form-field">
-                <input type="text" value={this.state.passport_id} onChange={this.handlePassIdChange} className="form-field-input"/> Password ID
-                <input type="text" value={this.state.name} onChange={this.handleNameChange} className="form-field-input"/> Name
-                <input type="text" value={this.state.surname} onChange={this.handleSurnameChange} className="form-field-input"/> Surname
-                <input type="text" value={this.state.id_code} onChange={this.handleIdCodeChange} className="form-field-input"/> Identification code
-                <input type="text" value={this.state.passport_pass} onChange={this.handlePassportPassChange} className="form-field-input"/> Passport password
-                <button>Save</button>
-                <Link to="/">Home</Link>
+                <div className="form">
+                    <label>
+                        <span>Password ID</span>
+                        <input type="text" value={this.state.passport_id} onChange={this.handlePassIdChange} className="form-field-input"/>
+                    </label>
+                    <label>
+                        <span>Name</span>
+                        <input type="text" value={this.state.name} onChange={this.handleNameChange} className="form-field-input"/>
+                    </label>
+                    <label>
+                        <span>Surname</span>
+                        <input type="text" value={this.state.surname} onChange={this.handleSurnameChange} className="form-field-input"/>
+                    </label>
+                    <label>
+                        <span>Identification code</span>
+                        <input type="text" value={this.state.id_code} onChange={this.handleIdCodeChange} className="form-field-input"/>
+                    </label>
+                    <label>
+                        <span>Passport password</span>
+                        <input type="text" value={this.state.passport_pass} onChange={this.handlePassportPassChange} className="form-field-input"/>
+                    </label>
+
+                    <button>Save</button>
+
+                </div>
             </form>
         )
     }
--- react_/src/components/Main.js
+++ react_/src/components/Main.js
@@ -20,6 +20,7 @@ class Main extends Component {
                 </header>
                 <nav className="main-nav">
                     <ul>
+                        <Link to="/" >Home</Link>
                         <Link to="/"><li className="main-nav-order">Электронное голосование</li></Link>
                         <Link to="/"><li className="main-nav-order">Новости</li></Link>
                         <Link to="/form"><li className="main-nav-order">Личный кабинет</li></Link>
